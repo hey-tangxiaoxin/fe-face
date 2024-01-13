aeb289d310a363bd8b67722f8862291599e61639
tangxiaoxin <1302947749@qq.com>
2024-01-13 16:30:21 +0800

    二叉树

---
+++ src/算法/二叉树/levelOrder.js
@@ -0,0 +1,26 @@
+/**
+ * 二叉树层序遍历
+ * @param {*} root 
+ * @returns 
+ */
+const levelOrder = (root) => {
+    if(!root) return []
+    const levels = []
+    const queue = [root]
+    while(queue.length){
+        const levelSize = queue.length;
+        const levelNodes = []
+        for(let i = 0;i<levelSize;i++){
+            const node = queue.shift()
+            levelNodes.push(node.val)
+            if(node.left){
+                queue.push(node.left)
+            }
+            if(node.right){
+                queue.push(node.right)
+            }
+        }
+        levels.push(levelNodes)
+    }
+    return levels
+}
