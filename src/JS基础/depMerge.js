a0bffd9f5b8bde3700ba98f79b1e96880ffdbf93
tangxiaoxin <1302947749@qq.com>
2024-01-22 14:15:02 +0800

    merge

---
+++ src/JS基础/depMerge.js
@@ -0,0 +1,52 @@
+function isObject(val) {
+  return Object.prototype.toString.call(val) === '[object Object]';
+}
+/**
+ * 对象深度合并
+ * @param {Object} target 
+ * @param {Object} source 
+ * @returns 
+ */
+function deepMerge(target, source) {
+  if (!isObject(target) || !isObject(source)) {
+    return source;
+  }
+  const targetObject = JSON.parse(JSON.stringify(target));
+  const sourceObject = JSON.parse(JSON.stringify(source));
+  Object.keys(sourceObject).forEach((key) => {
+    const targetValue = targetObject[key];
+    const sourceValue = sourceObject[key];
+    if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
+      targetObject[key] = targetValue.concat(sourceValue);
+    } else if (isObject(targetValue) && isObject(sourceValue)) {
+      targetObject[key] = deepMerge(targetValue, sourceValue);
+    } else {
+      targetObject[key] = sourceValue;
+    }
+  });
+  return targetObject;
+}
+
+const countries = {
+  USA: {
+    capital: "Washington D.C.",
+    emoji: "🇺🇸",
+    population: 331000000,
+  },
+};
+
+const countriesDetails = {
+  USA: {
+    language: "English",
+    currency: "USD",
+  },
+  Germany: {
+    capital: "Berlin",
+    emoji: "🇩🇪",
+    language: "German",
+    currency: "EUR",
+    population: 83000000,
+  },
+};
+
+console.log(deepMerge(countries, countriesDetails));
