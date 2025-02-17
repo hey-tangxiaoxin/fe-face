91802712051823ff1ab6bb79e8d8b69198ed3b3b
tangxiaoxin <1302947749@qq.com>
2025-02-17 13:04:50 +0800

    feat: 2.9

--- src/场景编程/LRU.ts
+++ src/场景编程/LRU.ts
@@ -1,30 +1,30 @@
 class LRU<T extends unknown> {
-    maxSize: number
-    dataMap: Map<string, T>
-    constructor(maxSize: number) {
-        this.maxSize = maxSize
-        this.dataMap = new Map<string, T>()
+  maxSize: number;
+  dataMap: Map<string, T>;
+  constructor(maxSize: number) {
+    this.maxSize = maxSize;
+    this.dataMap = new Map<string, T>();
+  }
+  get(k: string) {
+    if (!this.dataMap.has(k)) {
+      throw Error("not found");
     }
-    get(k: string) {
-        if (!this.dataMap.has(k)) {
-            throw Error('not found')
-        }
-        const val = this.dataMap.get(k)
-        this.dataMap.delete(k)
-        this.dataMap.set(k, val)
-        return val
+    const val = this.dataMap.get(k);
+    this.dataMap.delete(k);
+    this.dataMap.set(k, val);
+    return val;
+  }
+  set(k: string, val: T) {
+    if (this.dataMap.has(k)) {
+      this.dataMap.delete(k);
     }
-    set(k: string, val: T) {
-        if (this.dataMap.has(k)) {
-            this.dataMap.delete(k)
-        }
-        this.dataMap.set(k, val)
-        if (this.maxSize < this.dataMap.size) {
-            const deleteKey = this.dataMap.keys().next().value
-            this.dataMap.delete(deleteKey)
-        }
+    this.dataMap.set(k, val);
+    if (this.maxSize < this.dataMap.size) {
+      const deleteKey = this.dataMap.keys().next().value;
+      this.dataMap.delete(deleteKey);
     }
-    has(k: string) {
-        return this.dataMap.has(k)
-    }
+  }
+  has(k: string) {
+    return this.dataMap.has(k);
+  }
 }
